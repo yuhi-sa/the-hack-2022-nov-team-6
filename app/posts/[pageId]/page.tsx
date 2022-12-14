5f2f933e3a7484063bcdd2139cd15b0d8e302cab
neon <spka2yb9@gmail.com>
2022-12-14 21:54:39 +0900

    fix: テンプレートを参考にスタイルを付与

--- app/posts/[pageId]/page.tsx
+++ app/posts/[pageId]/page.tsx
@@ -9,19 +9,16 @@ type params = {
 async function Post(params: params) {
   const post = await getPostData(Number(params.params.pageId))
   return (
-    <div>
+    <div className='max-w-4xl mx-auto'>
       <div>
-        <h3 className='font-bold'>{post.title}</h3>
-        <p className='text-sm mt-3'>
-          <time dateTime='2022-12-02T11:30:00.000Z' className='mr-4'>
-            {post.publishedAt}
-          </time>
-          <span>■ カテゴリー A</span>
-        </p>
+        <h3 className='font-bold text-xl text-center mt-10'>{post.title}</h3>
+        <div className='meta flex gap-4'>
+          <p>{post.publishedAt}</p>
+        </div>
       </div>
       <img src={post.thumbnail} alt='thumbnail' className='aspect-[3/2] object-cover rounded-lg' />
       <section
-        className='prose  lg:prose-xl prose-stone'
+        className='prose lg:prose-xl prose-stone'
         dangerouslySetInnerHTML={{ __html: post.html }}
       ></section>
     </div>
